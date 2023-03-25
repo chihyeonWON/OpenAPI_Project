f296c8ca31f79a2b4eeabdcc789df7fa017ea1d9
ylose <heungeob1003@gmail.com>
2023-03-25 12:28:09 +0900

    http 요청하고 응답 처리

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,307 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:xml/xml.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '코로나 현황',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primaryColor: const Color(0xFF264653),
+        primaryColorDark: const Color(0xFF2A9D8F),
+        primaryColorLight: const Color(0xFFE9C46A),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class CovidItem {
+  String ? resultCode;
+  String ? resultMsg;
+  String ? numOfRows;
+  String ? pageNo;
+  String ? totalCount;
+  String ? ykiho;
+  String ? yadmNm;
+  String ? clCd;
+  String ? addr;
+  String ? asmGrd01;
+  String ? stateDt;
+  String ? stateTime;
+  String ? updateDt;
+  String ? createDt;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  CovidItem(
+      {this.resultCode,
+        this.resultMsg,
+        this.numOfRows,
+        this.pageNo,
+        this.totalCount,
+        this.ykiho,
+        this.yadmNm,
+        this.clCd,
+        this.addr,
+        this.asmGrd01,
+        this.stateDt,
+        this.stateTime,
+        this.updateDt,
+        this.createDt});
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MainPage extends StatefulWidget {
+  MainPageState createState() => MainPageState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MainPageState extends State<MainPage> {
+  XmlDocument? CovidStatusXML;
+  CovidItem covidItem = new CovidItem();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("코로나 현황"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.sync),
+            onPressed: () async {
+              const url =
+                  "http://apis.data.go.kr/B551182/hospAsmInfoService/getHospAsmInfo?serviceKey=c1ISMi%2BObOku4US5IhRtwjn5ZYQ3INLTkaqAYaOhkdArfvUx2HzAN%2Bj4w86CivGR7tUT9UJe1C9q5wEZXZW2bw%3D%3D&numOfRows=5&pageNo=1&ykiho=JDQ4MTg4MSM1MSMkMSMkMCMkODkkMzgxMzUxIzExIyQxIyQzIyQ4OSQyNjE4MzIjNTEjJDEjJDYjJDgz";
+              var response = await http.get(Uri.parse(url));
+
+              // print(response.body);
+              CovidStatusXML = XmlDocument.parse(response.body);
+              // print(response.body);
+              final items = CovidStatusXML!.findAllElements('item');
+              print(items);
+
+              items.forEach((element) {
+                covidItem = CovidItem(
+                  resultCode: element.getElement('resultCode')!.text,
+                  resultMsg: element.getElement('resultMsg')!.text,
+                  numOfRows: element.getElement('numOfRows')!.text,
+                  pageNo: element.getElement('pageNo')!.text,
+                  totalCount: element.getElement('totalCount')!.text,
+                  ykiho: element.getElement('ykiho')!.text,
+                  yadmNm: element.getElement('yadmNm')!.text,
+                  clCd: element.getElement('examCnt')!.text,
+                  addr: element.getElement('resutlNegCnt')!.text,
+                  asmGrd01: element.getElement('seq')!.text,
+                );
+              });
+            },
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomRight,
+              stops: [
+                0.8,
+                0.99,
+              ],
+              colors: [
+                Theme.of(context).primaryColorLight,
+                Theme.of(context).backgroundColor,
+              ]),
+        ),
+        child: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: _CovidItemPanel(
+                      title: "확진자",
+                      value: covidItem.yadmNm ?? "",
+                      textStyle: TextStyle(
+                        fontSize: 36,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: _CovidItemPanel(
+                      title: "격리해제",
+                      value: covidItem.totalCount.toString(),
+                      textStyle: TextStyle(
+                        fontSize: 36,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: _CovidItemPanel(
+                      title: "검사진행",
+                      value: covidItem.clCd,
+                      textStyle: TextStyle(
+                        fontSize: 36,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: _CovidItemPanel(
+                      title: "사망자",
+                      value: covidItem.ykiho,
+                      textStyle: TextStyle(
+                        fontSize: 36,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: _CovidItemPanel(
+                      title: "치료중인 환자",
+                      value: covidItem.pageNo,
+                      textStyle: TextStyle(
+                        fontSize: 36,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: _CovidItemPanel(
+                      title: "결과 음성",
+                      value: covidItem.addr.toString(),
+                      textStyle: TextStyle(
+                        fontSize: 32,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: _CovidItemPanel(
+                      title: "누적검사",
+                      value: covidItem.resultMsg,
+                      textStyle: TextStyle(
+                        fontSize: 32,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: _CovidItemPanel(
+                      title: "누적 검사 완료",
+                      value: covidItem.numOfRows,
+                      textStyle: TextStyle(
+                        fontSize: 32,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: _CovidItemPanel(
+                      title: "누적 환진률",
+                      textStyle: TextStyle(
+                        fontSize: 36,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: _CovidItemPanel(
+                      title: "등록일",
+                      value: covidItem.createDt,
+                      textStyle: TextStyle(
+                        fontSize: 36,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+class _CovidItemPanel extends StatefulWidget {
+  _CovidItemPanel({Key? key, this.title, this.value, this.textStyle}): super(key: key);
+
+  String? title;
+  String? value;
+  TextStyle? textStyle;
+
+  @override
+  _CovidItemPanelState createState() => _CovidItemPanelState();
+}
+
+class _CovidItemPanelState extends State<_CovidItemPanel> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(4),
+      padding: EdgeInsets.all(16),
+      height: 110,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [0.7, 0.9],
+              colors: [
+                Theme.of(context).primaryColor,
+                Theme.of(context).primaryColor.withAlpha(180),
+              ],
+              tileMode: TileMode.repeated),
+          boxShadow: [
+            BoxShadow(
+                offset: const Offset(5, 5),
+                blurRadius: 5,
+                spreadRadius: 1),
+          ]),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.only(bottom: 8),
+            child: Text(
+              widget.title.toString(),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Text(
+            widget.value.toString(),
+            style: widget.textStyle,
+          )
+        ],
+      ),
     );
   }
 }
